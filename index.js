6dc2f63f975cd6482e007552102f16144df7e19c
Alain Alisca <alainalisca@gmail.com>
2022-08-13 13:28:29 -0400

    commit tree aed4dddd6e6cde568f5192812ceefa815ccbc89c

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+    console.log ("TOAST" + input)
+    input.addEventListener('click', function () {
+        alert('I was clicked!')
+    })
 }
+
+// addingEventListener(); 
